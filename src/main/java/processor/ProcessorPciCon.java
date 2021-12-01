f2b1eb355fed853e092d69a51a6e71f64d511655
Grifak <vadim-nepomnyaschii@mail.ru>
2021-12-01 17:21:52 +0300

    feat: created processors with PCI and SWITCH connection

---
+++ src/main/java/processor/ProcessorPciCon.java
@@ -0,0 +1,9 @@
+package processor;
+
+public class ProcessorPciCon extends Processor{
+
+    @Override
+    void runProgram(int cntCommands, int cntMemCommands) {
+
+    }
+}
---
+++ src/main/java/processor/ProcessorSwitchCon.java
@@ -0,0 +1,8 @@
+package processor;
+
+public class ProcessorSwitchCon extends Processor{
+    @Override
+    void runProgram(int cntCommands, int cntMemCommands) {
+        
+    }
+}
