a92b7449a650bcbdefd2e94d7f294ce874553ad6
Grifak <vadim-nepomnyaschii@mail.ru>
2021-12-01 17:18:31 +0300

    feat: created Schema

---
+++ src/main/java/schema/Schema.java
@@ -0,0 +1,18 @@
+package schema;
+
+import memory.Memory;
+import processor.Processor;
+
+public abstract class Schema {
+    private Processor CPU_1;
+    private Processor CPU_2;
+    private Processor CPU_3;
+    private Processor CPU_4;
+
+    private Memory memory_1;
+    private Memory memory_2;
+    private Memory memory_3;
+    private Memory memory_4;
+
+
+}
