18fd1bb3e45faef154d0d17e98af2517b503f984
Grifak <vadim-nepomnyaschii@mail.ru>
2021-12-19 21:00:10 +0300

    refactor: time type

--- src/main/java/schema/Schema.java
+++ src/main/java/schema/Schema.java
@@ -12,7 +12,7 @@ public abstract class Schema {
     private final Integer cntCommands = 1000;
     private Integer cmdPercent;
     private Integer memPercent;
-    private Float time;
+    private Integer time;
 
     private Processor CPU_1;
     private Processor CPU_2;
@@ -46,8 +46,6 @@ public abstract class Schema {
         memory_4 = schemaFactory.getMemory();
     }
 
-    protected abstract SchemaFactory makeSchemaFactory();
-
     public void runSchema(){
         CPU_1.run();
         CPU_2.run();
@@ -55,11 +53,77 @@ public abstract class Schema {
         CPU_4.run();
     }
 
-    public Float getTime() {
+    protected abstract SchemaFactory makeSchemaFactory();
+
+    public Integer getTime() {
         return time;
     }
 
-    public void setTime(Float time) {
+    public void setTime(Integer time) {
         this.time = time;
+    }
+
+    public Processor getCPU_1() {
+        return CPU_1;
+    }
+
+    public void setCPU_1(Processor CPU_1) {
+        this.CPU_1 = CPU_1;
+    }
+
+    public Processor getCPU_2() {
+        return CPU_2;
+    }
+
+    public void setCPU_2(Processor CPU_2) {
+        this.CPU_2 = CPU_2;
+    }
+
+    public Processor getCPU_3() {
+        return CPU_3;
+    }
+
+    public void setCPU_3(Processor CPU_3) {
+        this.CPU_3 = CPU_3;
+    }
+
+    public Processor getCPU_4() {
+        return CPU_4;
+    }
+
+    public void setCPU_4(Processor CPU_4) {
+        this.CPU_4 = CPU_4;
+    }
+
+    public Memory getMemory_1() {
+        return memory_1;
+    }
+
+    public void setMemory_1(Memory memory_1) {
+        this.memory_1 = memory_1;
+    }
+
+    public Memory getMemory_2() {
+        return memory_2;
+    }
+
+    public void setMemory_2(Memory memory_2) {
+        this.memory_2 = memory_2;
+    }
+
+    public Memory getMemory_3() {
+        return memory_3;
+    }
+
+    public void setMemory_3(Memory memory_3) {
+        this.memory_3 = memory_3;
+    }
+
+    public Memory getMemory_4() {
+        return memory_4;
+    }
+
+    public void setMemory_4(Memory memory_4) {
+        this.memory_4 = memory_4;
     }
 }
