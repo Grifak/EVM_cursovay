436d3705c5073c48900855658f2c53bac39bb065
Grifak <vadim-nepomnyaschii@mail.ru>
2021-12-19 20:34:05 +0300

    refactor: processors

--- src/main/java/processor/ProcessorPciCon.java
+++ src/main/java/processor/ProcessorPciCon.java
@@ -6,9 +6,4 @@ public class ProcessorPciCon extends Processor{
     public ProcessorPciCon(Integer cntNoMemoryCom, Integer cntOwnMemoryCom, Integer cntExternalMemoryCom, Memory ownMemory) {
         super(cntNoMemoryCom, cntOwnMemoryCom, cntExternalMemoryCom, ownMemory);
     }
-
-    @Override
-    void runProgram(int cntCommands, int cntMemCommands) {
-        System.out.println("Run program on PROCESSOR with PCI con");
-    }
 }
--- src/main/java/processor/ProcessorSwitchCon.java
+++ src/main/java/processor/ProcessorSwitchCon.java
@@ -6,9 +6,4 @@ public class ProcessorSwitchCon extends Processor{
     public ProcessorSwitchCon(Integer cntNoMemoryCom, Integer cntOwnMemoryCom, Integer cntExternalMemoryCom, Memory ownMemory) {
         super(cntNoMemoryCom, cntOwnMemoryCom, cntExternalMemoryCom, ownMemory);
     }
-
-    @Override
-    public void runProgram(int cntCommands, int cntMemCommands) {
-        System.out.println("Run program on processor with SWITCH con");
-    }
 }
