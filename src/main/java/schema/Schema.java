ca26caa4f4c18066e06701dc12d95df56256dab4
Grifak <vadim-nepomnyaschii@mail.ru>
2021-12-19 18:52:24 +0300

    feat(Schema): calculate ownCommands, externalCommnads

--- src/main/java/schema/Schema.java
+++ src/main/java/schema/Schema.java
@@ -4,7 +4,14 @@ import factory.SchemaFactory;
 import memory.Memory;
 import processor.Processor;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public abstract class Schema {
+    private final Integer cntCommands = 1000;
+    private Integer cmdPercent;
+    private Integer memPercent;
     private Processor CPU_1;
     private Processor CPU_2;
     private Processor CPU_3;
@@ -15,17 +22,27 @@ public abstract class Schema {
     private Memory memory_3;
     private Memory memory_4;
 
-    public Schema() {
+    public Schema(Integer cmdPercent, Integer memPercent) {
+        MathContext context = new MathContext(5, RoundingMode.HALF_UP);
+        BigDecimal cmdValue = new BigDecimal((1 - cmdPercent/100.0), context);
+        BigDecimal memValue = new BigDecimal((1 - memPercent/100.0), context);
+
+        int noMemCommand = (int) (1000 * (cmdPercent/100.0));
+        int memCommand = (int) (1000 * cmdValue.doubleValue());
+        int cntOwnData = (int) (memCommand * (memPercent/100.0));
+        int cntExternalData = (int) (memCommand * memValue.doubleValue());
+        Boolean isBusy = false;
+
         SchemaFactory schemaFactory = makeSchemaFactory();
         CPU_1 = schemaFactory.getProcessor();
         CPU_2 = schemaFactory.getProcessor();
         CPU_3 = schemaFactory.getProcessor();
         CPU_4 = schemaFactory.getProcessor();
 
-        memory_1 = schemaFactory.getMemory();
-        memory_2 = schemaFactory.getMemory();
-        memory_3 = schemaFactory.getMemory();
-        memory_4 = schemaFactory.getMemory();
+        memory_1 = schemaFactory.getMemory(cntOwnData, cntExternalData, isBusy);
+        memory_2 = schemaFactory.getMemory(cntOwnData, cntExternalData, isBusy);
+        memory_3 = schemaFactory.getMemory(cntOwnData, cntExternalData, isBusy);
+        memory_4 = schemaFactory.getMemory(cntOwnData, cntExternalData, isBusy);
     }
 
     public void runSchema(){
