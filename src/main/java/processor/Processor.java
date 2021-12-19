1cc7746e0953920668c8c7da4fe6bc53e2ad8f6e
Grifak <vadim-nepomnyaschii@mail.ru>
2021-12-19 20:20:24 +0300

    refacrot(Processor)

--- src/main/java/processor/Processor.java
+++ src/main/java/processor/Processor.java
@@ -1,5 +1,46 @@
 package processor;
 
-public abstract class Processor extends Thread{
-    abstract void runProgram(int cntCommands, int cntMemCommands);
+import memory.Memory;
+
+public class Processor extends Thread{
+    private Status status;
+    private Integer cntNoMemoryCom;
+    private Integer cntOwnMemoryCom;
+    private Integer cntExternalMemoryCom;
+    private Memory ownMemory;
+
+    public Processor(Integer cntNoMemoryCom, Integer cntOwnMemoryCom, Integer cntExternalMemoryCom, Memory ownMemory){
+        this.cntNoMemoryCom = cntNoMemoryCom;
+        this.cntOwnMemoryCom = cntOwnMemoryCom;
+        this.cntExternalMemoryCom = cntExternalMemoryCom;
+        this.ownMemory = ownMemory;
+    }
+
+    public Integer getCntNoMemoryCom() {
+        return cntNoMemoryCom;
+    }
+
+    public void setCntNoMemoryCom(Integer cntNoMemoryCom) {
+        this.cntNoMemoryCom = cntNoMemoryCom;
+    }
+
+    public Integer getCntOwnMemoryCom() {
+        return cntOwnMemoryCom;
+    }
+
+    public void setCntOwnMemoryCom(Integer cntOwnMemoryCom) {
+        this.cntOwnMemoryCom = cntOwnMemoryCom;
+    }
+
+    public Integer getCntExternalMemoryCom() {
+        return cntExternalMemoryCom;
+    }
+
+    public void setCntExternalMemoryCom(Integer cntExternalMemoryCom) {
+        this.cntExternalMemoryCom = cntExternalMemoryCom;
+    }
+
+    public String getStatus(){
+        return this.status.getTitle();
+    }
 }
---
+++ src/main/java/processor/Status.java
@@ -0,0 +1,29 @@
+package processor;
+
+import java.util.Arrays;
+
+public enum Status {
+    WORK_WITH_MEMORY_1("WORK_WITH_MEMORY_1"),
+    WORK_WITH_MEMORY_2("WORK_WITH_MEMORY_2"),
+    WORK_WITH_MEMORY_3("WORK_WITH_MEMORY_3"),
+    WORK_WITH_MEMORY_4("WORK_WITH_MEMORY_4"),
+    WAIT("WAIT"),
+    FINISHED("FINISHED");
+
+    private String title;
+
+    Status(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static Status fromString(String value){
+        return Arrays.stream(Status.values())
+                .filter(s->s.title.equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(()-> new RuntimeException(String.format("Status %s not found", value)));
+    }
+}
